f739828256d2f61331bec19c79f30063c4ed6f0b
Oleg McNamara <oleg.mcnamara@p97.com>
2020-03-19 23:32:22 -0500

    Initial Commit

---
+++ ItsHappening/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItsHappening
+//
+//  Created by Oleg McNamara on 3/19/20.
+//  Copyright © 2020 Oleg McNamara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
