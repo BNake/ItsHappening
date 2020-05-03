38256ba1b60534a98c1a1dc9d59519bc2d8fa8bb
Oleg McNamara <omcnamar10@gmail.com>
2020-05-03 15:13:13 -0500

    refactor search on contact screen

--- ItsHappening/Modules/Contact/ContactViewModel.swift
+++ ItsHappening/Modules/Contact/ContactViewModel.swift
@@ -26,7 +26,7 @@ class ContactViewModel: BaseViewModel {
         return sectionViewModels.asDriver(onErrorJustReturn: [])
     }
     
-    private let friendsList = BehaviorRelay<[HappeningUser]>(value: [])
+    private let usersList = BehaviorRelay<[HappeningUser]>(value: [])
     
     init(router: ContactRouter) {
         self.router = router
@@ -36,76 +36,78 @@ class ContactViewModel: BaseViewModel {
     
     private func binding() {
         
+        //
         // bind the change in segment
+        //
         segmentControlChanged.bind { [weak self] (segmentedControl) in
             guard let self = self else { return }
-            switch segmentedControl {
-            case .all:
-                self.searchAlgolia(forText: self.searchText.value)
-            case .friends:
-                self.showFriends()
-            }
+            self.setMode(segment: segmentedControl, inputString: self.searchText.value)
         }.disposed(by: disposeBag)
         
+        
+        //
         // subscribe to searchbox
+        //
         searchText
-            .throttle(RxTimeInterval.milliseconds(1000), scheduler: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] (queries) in
+            // .throttle(RxTimeInterval.milliseconds(1000), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] (inputString) in
                 guard let self = self else { return }
-                switch self.segmentControlChanged.value {
-                case.all:
-                    self.searchAlgolia(forText: queries)
-                case .friends:
-                    break
-                }
-                
+                self.setMode(segment: self.segmentControlChanged.value, inputString: inputString)
         }).disposed(by: disposeBag)
         
-        // subscribe to friends list
-        friendsList.bind { [weak self] (users) in
+        
+        //
+        // subscribe to users list
+        //
+        usersList.bind { [weak self] (users) in
             guard let self = self else { return }
-            self.reloadTableView(rows: self.makeRows(from: users))
+            let rows = self.makeRows(from: users)
+            self.reloadTableView(rows: rows)
         }.disposed(by: disposeBag)
     }
     
-    func searchAlgolia(forText searchString : String) {
+    func setMode(segment: SegmentControl, inputString: String) {
+        switch segment {
+        case.all:
+            self.searchAlgolia(forText: inputString) { (users) in
+                self.usersList.accept(users)
+            }
+        case .friends:
+            self.searchAlgolia(forText: inputString) { (users) in
+                guard let loggedInUser = FirebaseAuthService
+                                                    .sharedInstance
+                                                    .loggedInUser
+                                                    .value else {
+                        self.usersList.accept([])
+                        return
+                }
+                
+                let friendsOnly = users.filter { (user) -> Bool in
+                    return loggedInUser.idsOfUsersFollowing.contains(user.id)
+                }
+                self.usersList.accept(friendsOnly)
+            }
+        }
+    }
+    
+    func searchAlgolia(forText searchString : String, result: @escaping ([HappeningUser]) -> Void) {
         
         let usersTable = FirebaseFireStoreService<HappeningUser>(collectionName: "Users")
 
         usersTable.search(queryString: searchString,
-                          attributesToRetrieve: ["firstName",
+                          attributesToRetrieve: ["id",
+                                                 "firstName",
                                                  "lastName",
                                                  "username",
                                                  "profileImageUrl"],
                           maxCount: 20,
-                          success: { [weak self] (users) in
-                               
-                            guard let self = self else { return }
-                            self.reloadTableView(rows: self.makeRows(from: users))
-                            
+                          success: { (users) in
+                               result(users)
                           }) { (error) in
                                debugPrint(error)
+                               result([])
                           }
         
-    }
-    
-    func showFriends() {
-        
-        guard FirebaseAuthService.sharedInstance.isLoggedIn() else { return }
-        guard let user = FirebaseAuthService.sharedInstance.loggedInUser.value else { return }
-        friendsList.accept([])
-        
-        let usersTable = FirebaseFireStoreService<HappeningUser>(collectionName: "Users")
-        for id in user.idsOfUsersFollowing {
-            usersTable.getDocument(documnetId: id, success: { [weak self] (user) in
-                guard let self = self else { return }
-                var friends = self.friendsList.value
-                friends.append(user)
-                self.friendsList.accept(friends)
-            }) { (error) in
-                debugPrint(error)
-            }
-        }
     }
     
     private func makeRows(from users: [HappeningUser]) -> [ContactCellViewModel] {
